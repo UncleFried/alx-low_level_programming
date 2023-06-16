b48eeef11a9a6c8dab3722e992348764fd2cfe7b
UncleFried <frimponjoshua@gmail.com>
2023-06-16 08:48:14 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,13 @@
 *
 *Return:Always 0 (Success)
 */
-int main() {
-printf("Size of char: %d byte(s)\n", sizeof(char));
-printf("Size of int: %d byte(s)\n", sizeof(int));
-printf("Size of long int: du byte(s)\n", sizeof(long int));
-printf("Size of long long: %d byte(s)\n", sizeof(long long int));
-printf("Size of float: %d byte(s)\n", sizeof(float));
-return 0;
+int main(void)
+{
+printf("Size a of char: %d byte(s)\n", sizeof(char));
+printf("Size a of int: %d byte(s)\n", sizeof(int));
+printf("Size a of long int: du byte(s)\n", sizeof(long int));
+printf("Size a of long long: %d byte(s)\n", sizeof(long long int));
+printf("Size a of float: %d byte(s)\n", sizeof(float));
+return (0);
 }
 
